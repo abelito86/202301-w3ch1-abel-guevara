38595d8c03d0f1d8ddc4d8ef21164459fe31d4d0
Abel <abelguevaralanda@yahoo.es>
2023-01-31 00:21:52 +0100

    Add out-of-stock-function

--- public/js/src/no-stock.test.js
+++ public/js/src/no-stock.test.js
@@ -1,6 +1,5 @@
 import { noStockProduct } from './no-stock.js';
 import { objectProducts } from './data.js';
-
 test('Print test', () => {
-  expect(noStockProduct(objectProducts)).toBe('Hay 0 productos sin stock');
+    expect(noStockProduct(objectProducts)).toBe('Hay 0 productos sin stock');
 });
